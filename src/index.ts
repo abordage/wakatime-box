7132b06da87540036609344bf209aeb3abfce534
Pavel Bychko <abordage.dev@gmail.com>
2022-05-31 14:46:01 +0300

    add support for GitHub Job Summaries

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import * as core from '@actions/core';
+import {HttpClient} from '@actions/http-client';
 import {Octokit} from '@octokit/rest';
 import {config} from 'dotenv';
 import {resolve} from 'path';
 import convertSeconds from './convertSeconds';
 import generateBarChart from './generateBarChart';
-const axios = require('axios');
-
 
 config({path: resolve(__dirname, '../.env')});
+
 const GH_TOKEN = process.env.GH_TOKEN || core.getInput('GH_TOKEN', {required: true});
 const WAKA_API_KEY = process.env.WAKA_API_KEY || core.getInput('WAKA_API_KEY', {required: true});
 const GIST_ID = process.env.GIST_ID || core.getInput('GIST_ID', {required: true});
-const MAX_RESULT = core.getInput('MAX_RESULT', {required: false});
+const MAX_RESULT = Number(core.getInput('MAX_RESULT', {required: false})) || 5;
 const DATE_RANGE = core.getInput('DATE_RANGE', {required: false});
 
 let range: string = DATE_RANGE;
@@ -21,48 +21,37 @@ if (!['last_7_days', 'last_30_days', 'last_6_months', 'last_year'].includes(rang
   /**
    * Get statistics
    */
-  const response = await axios.get('users/current/stats/' + range, {
-    baseURL: 'https://wakatime.com/api/v1/',
-    headers: {Authorization: `Basic ${Buffer.from(WAKA_API_KEY || '').toString('base64')}`},
-  }).catch(function (error: Error) {
-    core.setFailed(`Action failed with error ${error.message}`);
-  });
+  const httpClient = new HttpClient();
+  const response = await httpClient.getJson('https://wakatime.com/api/v1/users/current/stats/' + range,
+    {Authorization: `Basic ${Buffer.from(WAKA_API_KEY || '').toString('base64')}`})
+    .catch(error => core.setFailed(`Action failed with error ${error.message}`));
+
+  // @ts-ignore
+  const languages: any[] = response.result.data.languages;
+  if (!languages) {
+    core.setFailed('Action failed (empty response)');
+    return;
+  }
 
   /**
    * Formatting
    */
-  const myStats = response.data.data;
-  const maxResult: number = Number(MAX_RESULT) || 5;
-
   let allOtherTime = 0;
   let allOtherPercent = 0;
-
-  const lines = myStats.languages.reduce((prev: any[], cur: any) => {
+  const lines = languages.reduce((prev: any[], cur: any) => {
     const {name, percent, total_seconds} = cur;
-    const line = [
-      cutStr(name, 10).padEnd(12),
-      convertSeconds(total_seconds).padEnd(11),
-      generateBarChart(percent, 21),
-      String(percent.toFixed(1)).padStart(5) + '%',
-    ];
+    const line = formatLine(name, total_seconds, percent);
 
-    if (name == 'Other' || prev.length >= maxResult - 1) {
-      allOtherTime = allOtherTime + total_seconds;
-      allOtherPercent = allOtherPercent + percent;
+    if (name == 'Other' || prev.length >= MAX_RESULT - 1) {
+      allOtherTime += total_seconds;
+      allOtherPercent += percent;
       return prev;
     }
 
-    return [...prev, line.join(' ')];
+    return [...prev, line];
   }, []);
 
-  const lastLine = [
-    cutStr('Other lang', 10).padEnd(12),
-    convertSeconds(allOtherTime).padEnd(11),
-    generateBarChart(allOtherPercent, 21),
-    String(allOtherPercent.toFixed(1)).padStart(5) + '%',
-  ];
-  lines.push(lastLine.join(' '));
-
+  lines.push(formatLine('Other lang', allOtherTime, allOtherPercent));
   if (lines.length === 0) return;
 
   let title: string = 'Latest';
@@ -89,19 +78,29 @@ if (!['last_7_days', 'last_30_days', 'last_6_months', 'last_year'].includes(rang
     },
   }).catch(error => core.setFailed(`Action failed with error: Gist ${error.message}`));
 
-
-  await core.summary
-    .addHeading('Results')
-    .addTable([
-      [{data: 'Action', header: true}, {data: 'Result', header: true}],
-      ['Statistics received', 'Pass '],
-      ['Gist updated', 'Pass ']
-    ])
-    .addBreak()
-    .addLink('wakatime-gist', 'https://github.com/marketplace/actions/wakatime-gist')
-    .write();
+  if (process.env.ACTION_ENV !== 'local') {
+    await core.summary
+      .addHeading('Results')
+      .addTable([
+        [{data: 'Action', header: true}, {data: 'Result', header: true}],
+        ['Statistics received', '✔ Pass'],
+        ['Gist updated', '✔ Pass']
+      ])
+      .addBreak()
+      .addLink('wakatime-gist', 'https://github.com/marketplace/actions/wakatime-gist')
+      .write();
+  }
 })();
 
 function cutStr(str: string, len: number) {
   return str.length > len ? str.substring(0, len - 3) + '...' : str;
+}
+
+function formatLine(name: string, total_seconds: number, percent: number) {
+  return [
+    cutStr(name, 10).padEnd(12),
+    convertSeconds(total_seconds).padEnd(11),
+    generateBarChart(percent, 21),
+    String(percent.toFixed(1)).padStart(5) + '%',
+  ].join(' ');
 }
